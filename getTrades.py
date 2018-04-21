4cf3328cccb6d5e7ef4cee264aeb485abe7fda47
nvalidov <niyazvalidov@gmail.com>
2018-04-22 01:36:53 +0300

    pushTrade

--- getTrades.py
+++ getTrades.py
@@ -1,14 +1,13 @@
 from solc import compile_source
 from web3 import Web3, HTTPProvider
 from web3.contract import ConciseContract
+import time
 
 web3 = Web3(HTTPProvider('http://localhost:9545'))
 
 contract_address = "0x345ca3e014aaf5dca488057592ee47305d9b3e10"
-trader_address = "0xf17f52151ebef6c7334fad080c5704d77216b732"
 
 contract_address = web3.toChecksumAddress(contract_address)
-trader_address = web3.toChecksumAddress(trader_address)
 
 contract_fs = open('smart-contract/contracts/Trades.sol', 'r')
 contract_source_code = ''.join(contract_fs.readlines())
@@ -20,7 +19,22 @@ contract_interface = compiled_sol['<stdin>:Trades']
 instance = web3.eth.contract(abi=contract_interface['abi'], address=contract_address,
                                       ContractFactoryClass=ConciseContract)
 
-def push(_type, _shareName, _cost, _count, _comission, _time, _signature):
-	instance.addTrade(_type, _shareName, _cost, _count, _comission, _time, _signature, transact={'from': trader_address})
+contract = web3.eth.contract(address=contract_address, abi=contract_interface['abi'])
 
-push('sell', 'SBR', 10000, 1, 1, 123, 'sign')
+def sendToBack(trade):
+	print(trade)
+
+
+last_count = 0;
+
+def getTrades():
+	global last_count
+	while True:
+		count = instance.getSize()
+		for i in range(last_count, count):
+			sendToBack(instance.getTrade(i))
+		last_count = count
+		time.sleep(2)
+
+getTrades()
+
